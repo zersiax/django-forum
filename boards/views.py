2eafeb004bd308502ecbac6d3a6d7f1e6a7f01a8
zersiax <florianbeijers@gmail.com>
2017-10-10 22:34:35 +0200

    added a topics template, needs refactoring to use reusable templates. Also wrote topics test and wrote and refactored the READ view a couple times. Tired now *zzz*

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import Board
+
+
 def home(request):
     boards = Board.objects.all()
     return render(request, 'home.html', {'boards': boards})
-    
+
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk) # refactored from a try-catch block, cleaner this way
+    return render(request, 'topics.html', {'board': board})
