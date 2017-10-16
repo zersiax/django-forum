8eeba7a9cfa4d4428b6d17f43b7b995efa6a8f45
zersiax <florianbeijers@gmail.com>
2017-10-17 00:25:10 +0200

    Started on the authentication logic for this application. Interesting that my tests error out, rather than fail. This requires some more research

--- boards/tests.py
+++ boards/tests.py
@@ -5,22 +5,27 @@ from django.contrib.auth.models import User
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
 from .forms import NewTopicForm
+
+
 class HomeTests(TestCase):
     def setUp(self):
-        self.board = Board.objects.create(name='Django', description='Django board')
+        self.board = Board.objects.create(
+            name='Django', description='Django board')
         url = reverse('home')
         self.response = self.client.get(url)
-    
+
     def test_home_view_status_code(self):
         self.assertEquals(self.response.status_code, 200)
 
     def test_home_url_resolves_home_view(self):
         view = resolve('/')
         self.assertEquals(view.func, home)
-    
+
     def test_home_view_contains_link_to_topics_page(self):
-        board_topics_url = reverse('board_topics', kwargs={'pk': self.board.pk})
-        self.assertContains(self.response, 'href="{0}"'.format(board_topics_url))
+        board_topics_url = reverse(
+            'board_topics', kwargs={'pk': self.board.pk})
+        self.assertContains(
+            self.response, 'href="{0}"'.format(board_topics_url))
 
 
 class BoardTopicsTests(TestCase):
@@ -41,17 +46,19 @@ class BoardTopicsTests(TestCase):
     def test_board_topics_url_resolves_board_topics_view(self):
         view = resolve('/boards/1/')
         self.assertEquals(view.func, board_topics)
-    
+
     def test_board_topics_view_contains_link_back_to_homepage(self):
-        board_topics_url =  reverse('board_topics', kwargs={ 'pk': 1})
+        board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(board_topics_url)
         homepage_url = reverse('home')
         self.assertContains(response, 'href="{0}"'.format(homepage_url))
-    
+
+
 class NewTopicTests(TestCase):
     def setUp(self):
         Board.objects.create(name='Django', description='Django board.')
-        User.objects.create_user(username='john', email='john@doe.com', password='123')
+        User.objects.create_user(
+            username='john', email='john@doe.com', password='123')
 
     def test_new_topic_view_success_status_code(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -72,7 +79,7 @@ class NewTopicTests(TestCase):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         response = self.client.get(new_topic_url)
         self.assertContains(response, 'href="{0}"'.format(board_topics_url))
-    
+
     def test_board_topics_view_contains_navigation_links(self):
         board_topics_url = reverse('board_topics', kwargs={'pk': 1})
         homepage_url = reverse('home')
@@ -98,7 +105,7 @@ class NewTopicTests(TestCase):
         self.assertTrue(Topic.objects.exists())
         self.assertTrue(Post.objects.exists())
 
-        def test_new_topic_invalid_post_data(self):  
+    def test_new_topic_invalid_post_data(self):
         '''
         Invalid post data should not redirect
         The expected behavior is to show the form again with validation errors
@@ -108,6 +115,7 @@ class NewTopicTests(TestCase):
         form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
         self.assertTrue(form.errors)
+
     def test_new_topic_invalid_post_data_empty_fields(self):
         '''
         Invalid post data should not redirect
@@ -123,9 +131,8 @@ class NewTopicTests(TestCase):
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
 
-    def test_contains_form(self):  
+    def test_contains_form(self):
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.get(url)
         form = response.context.get('form')
         self.assertIsInstance(form, NewTopicForm)
-
