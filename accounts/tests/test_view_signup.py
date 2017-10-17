da56d663b074edc7b7ed5e5f869bb796c04d052b
zersiax <florianbeijers@gmail.com>
2017-10-17 15:51:05 +0200

    Going to make smaller commits from this point onwards for easier rollback. Added email to user creation by extending default UCF, than prettified the signup template and started rearchitecturing the tests since they were getting a bit fat

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.urls import resolve
-from .views import signup
-from django.contrib.auth.forms import UserCreationForm
+from ..views import signup
+from ..forms import SignUpForm
 from django.contrib.auth.models import User
 
 class SignUpTests(TestCase):
@@ -22,13 +22,24 @@ class SignUpTests(TestCase):
 
     def test_contains_form(self):
         form = self.response.context.get('form')
-        self.assertIsInstance(form, UserCreationForm)
+        self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
         url = reverse('signup')
         data = {
             'username': 'john',
+            'email': 'john@doe.com',
             'password1': 'abcdef123456',
             'password2': 'abcdef123456'
         }
--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django.shortcuts import render, redirect
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid:
             user = form.save()
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
 
